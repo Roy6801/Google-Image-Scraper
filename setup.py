c4bacda701a656c8c0f6a52455885b68f9628bfb
Roy6801 <>
2021-12-07 10:11:13 +0530

    update

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 DESCRIPTION = 'Google Image Scraper'
 LONG_DESCRIPTION = 'A package that allows to fetch image urls from google images.'
 
