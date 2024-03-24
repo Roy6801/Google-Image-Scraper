f974c8167afa53f85400680fc0a2ebc8cc3be61a
roy6801 <>
2024-03-25 00:25:40 +0530

    refactor handler and stream files

--- gi_scraper/handler.py
+++ gi_scraper/handler.py
@@ -1,7 +1,6 @@
-from typing import Literal, Union, Generator, Any
+from typing import Literal, Union
 from .util import query_cleaner
-from queue import Queue
-import time
+
 
 ResponseDict = dict[
     Literal[
@@ -98,89 +97,3 @@ class Response:
             "width": self.width,
             "height": self.height,
         }
-
-
-class QueueStream:
-    """
-    Represents a stream for processing items from a queue.
-
-    Parameters:
-    - queue (Queue): The queue to process items from.
-    - timeout (int): The timeout for waiting for items in the queue.
-    - initial_timeout (int): The initial timeout for waiting for items in the queue.
-
-    Attributes:
-    - __queue (Queue): The queue to process items from.
-    - __timeout (int): The timeout for waiting for items in the queue.
-    - __initial_timeout (int): The initial timeout for waiting for items in the queue.
-
-    Methods:
-    - get(): Generator method for retrieving items from the queue as Response objects.
-
-    Yields:
-    - Response: An instance of the Response class.
-    """
-
-    def __init__(
-        self, queue: Queue, timeout: int = 10, initial_timeout: int = 30
-    ) -> None:
-        self.__queue = queue
-        self.__timeout = timeout
-        self.__initial_timeout = initial_timeout
-
-    def get(self) -> Generator[Response, Any, None]:
-        """
-        Generator method for retrieving items from the queue as Response objects.
-
-        Yields:
-        - Response: An instance of the Response class.
-        """
-
-        last_fetch = time.time()
-        wait_time = 0
-
-        timeout = self.__initial_timeout
-
-        while wait_time < timeout:
-            if not self.__queue.empty():
-                timeout = self.__timeout
-                item = self.__queue.get()
-                if item is None:
-                    break
-                yield item
-                last_fetch = time.time()
-                wait_time = 0
-            else:
-                wait_time = time.time() - last_fetch
-
-
-class CacheStream:
-    """
-    Represents a stream for processing items from a cached list of responses.
-
-    Parameters:
-    - responses (list[ResponseDict]): The list of cached responses.
-
-    Attributes:
-    - __responses (list[ResponseDict]): The list of cached responses.
-
-    Methods:
-    - get(): Generator method for retrieving items from the cached responses as Response objects.
-
-    Yields:
-    - Response: An instance of the Response class.
-    """
-
-    def __init__(self, responses: list[ResponseDict]) -> None:
-        self.__responses = responses
-
-    def get(self) -> Generator[Response, Any, None]:
-        """
-        Generator method for retrieving items from the cached responses as Response objects.
-
-        Yields:
-        - Response: An instance of the Response class.
-        """
-
-        for item in self.__responses:
-            yield Response(**item)
